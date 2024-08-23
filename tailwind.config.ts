b6c7740573e69f34de0409ae6baacacea64fd9fa
vdsbenoit <vdsbenoit@gmail.com>
2024-08-23 10:57:20 +0200

    Define base colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 export default <Partial<Config>>{
   theme: {
     extend: {
       aspectRatio: {
-        auto: 'auto',
-        square: '1 / 1',
-        video: '16 / 9'
-      }
-    }
+        auto: "auto",
+        square: "1 / 1",
+        video: "16 / 9",
+      },
+      colors: {
+        frite: {
+          50: "#fdfce9",
+          100: "#fcf7c5",
+          200: "#fbec8d",
+          300: "#f8da4c",
+          400: "#f4c61f",
+          500: "#e4ad0e",
+          600: "#c58509",
+          700: "#9d5f0b",
+          800: "#824b11",
+          900: "#6e3e15",
+          950: "#401f08",
+        },
+      },
+    },
   },
   content: [
-    'docs/content/**/*.md'
-  ]
-}
+    "content/**/*.md",
+    "docs/content/**/*.md",
+  ],
+};
