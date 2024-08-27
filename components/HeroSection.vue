f30b31dc42f0ad97bc4150b762100f3ffb7223ff
vdsbenoit <vdsbenoit@gmail.com>
2024-08-27 18:12:09 +0200

    Draft HeroSection

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,7 +1,30 @@
 <template>
-  <div>
-    <h1 class="text-6xl font-extrabold tracking-tight">frite.dev</h1>
-    <h2 class="text-5xl">Belgian-grade software development</h2>
+  <div class="flex min-h-screen items-center justify-center">
+    <div class="text-center">
+      <img
+        class="mx-auto h-10 w-10"
+        src="~/assets/img/frites.png"
+        alt="frite-logo"
+      />
+      <p class="text-primary font-extrabold tracking-tight">frite.dev</p>
+      <h1 class="mt-4 text-5xl text-gray-100">
+        Belgian-grade<br />software services
+      </h1>
+      <div class="mt-6 grid grid-cols-1 gap-4 sm:grid-cols-2">
+        <a
+          href="#contact"
+          class="bg-primary inline-block rounded-lg px-6 py-3 font-semibold text-gray-800 shadow-lg"
+        >
+          Contact us
+        </a>
+        <a
+          href="#services"
+          class="inline-block rounded-lg bg-gray-800 px-6 py-3 font-semibold text-gray-100 shadow-lg"
+        >
+          Learn more
+        </a>
+      </div>
+    </div>
   </div>
 </template>
 
