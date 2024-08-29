5f2c10168a7a63a088b154c1ec56055c0aac3e7c
vdsbenoit <vdsbenoit@gmail.com>
2024-08-29 15:35:32 +0200

    Apply clipped background around hero title

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -7,14 +7,15 @@
         alt="frite-logo"
       />
       <p class="text-primary font-bold">frite.dev</p>
-      <h1 class="relative mt-3 px-6 pb-8 pt-4 text-5xl font-light">
+      <div id="background-clip" ref="backgroundClip"></div>
+      <h1 ref="heroTitle" class="relative mt-3 px-2 text-5xl font-light">
         <span
-          class="block bg-gradient-to-t from-slate-400 to-white bg-clip-text leading-normal text-transparent"
+          class="block bg-gradient-to-t from-gray-400 to-white bg-clip-text leading-normal text-transparent"
         >
           Empower your businesses
         </span>
         <span
-          class="block bg-gradient-to-t from-slate-400 to-white bg-clip-text text-transparent"
+          class="block bg-gradient-to-t from-gray-400 to-white bg-clip-text text-transparent"
         >
           with tailored software solutions
         </span>
@@ -39,6 +40,37 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const heroTitle = ref<HTMLInputElement | null>(null);
+const backgroundClip = ref<HTMLInputElement | null>(null);
 
-<style></style>
+const updateClipPath = () => {
+  const h1Element = heroTitle.value;
+  const bgClipElement = backgroundClip.value;
+
+  if (h1Element && bgClipElement) {
+    const rect = h1Element.getBoundingClientRect();
+    const clipPath = `polygon(${rect.left}px ${rect.top}px, ${rect.right}px ${rect.top}px, ${rect.right}px ${rect.bottom}px, ${rect.left}px ${rect.bottom}px)`;
+    bgClipElement.style.clipPath = clipPath;
+  }
+};
+onMounted(() => {
+  updateClipPath();
+  window.addEventListener("resize", updateClipPath);
+});
+onUnmounted(() => {
+  window.removeEventListener("resize", updateClipPath);
+});
+</script>
+
+<style scoped>
+#background-clip {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: radial-gradient(ellipse at bottom, #262626 0%, #0a0a0a 100%);
+  z-index: -1;
+}
+</style>
