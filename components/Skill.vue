fcdd4cbfa6176a19c4e45cb5eb15341b469bbc17
vdsbenoit <vdsbenoit@gmail.com>
2024-09-05 09:57:46 +0200

    First draft skills

---
+++ components/Skill.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="relative inline-block">
+    <div
+      class="group m-1 cursor-pointer border border-white"
+      @click="showDescription = !showDescription"
+    >
+      <div
+        class="flex size-16 scale-75 items-center justify-center rounded-full bg-gray-800 transition-transform ease-in-out group-hover:-translate-y-3 group-hover:scale-100"
+      >
+        <UIcon class="size-8" :name="icon" :style="{ color }" />
+      </div>
+      <div
+        class="absolute bottom-0 left-1/2 -translate-x-1/2 text-center opacity-0 transition ease-in-out group-hover:translate-y-3 group-hover:opacity-100"
+      >
+        <span
+          class="rounded p-1 text-xs text-gray-800"
+          :style="{ backgroundColor: color }"
+          >{{ title }}</span
+        >
+      </div>
+    </div>
+    <div
+      v-if="showDescription"
+      class="absolute left-full top-0 ml-4 rounded bg-gray-800 p-4 text-white"
+      :style="{ backgroundColor: color || '' }"
+    >
+      <h3 class="font-bold">{{ title }}</h3>
+      <p>{{ description }}</p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string;
+  icon: string;
+  color: string;
+  description: string;
+}>();
+
+const showDescription = ref(false);
+</script>
+
+<style scoped>
+/* Add any additional scoped styles here */
+</style>
--- components/section/Skills.vue
+++ components/section/Skills.vue
@@ -1,9 +1,104 @@
 <template>
-  <div class="flex min-h-screen items-center justify-center">
-    <h2>Skills</h2>
+  <div class="flex min-h-screen items-center justify-center px-4">
+    <div>
+      <h2 class="mb-8 text-left text-5xl font-bold">Skills</h2>
+      <div class="grid grid-cols-1 gap-8 md:grid-cols-3">
+        <div>
+          <h3 class="mb-4 text-xl font-semibold">Hard skills</h3>
+          <div class="space-y-4">
+            <Skill
+              v-for="skill in skills.hard"
+              :key="skill.title"
+              :title="skill.title"
+              :icon="skill.icon"
+              :description="skill.description"
+              :color="skill.color"
+            />
+          </div>
+        </div>
+        <div>
+          <h3 class="mb-4 text-xl font-semibold">Technologies</h3>
+          <div class="space-y-4">
+            <Skill
+              v-for="skill in skills.technologies"
+              :key="skill.title"
+              :title="skill.title"
+              :icon="skill.icon"
+              :description="skill.description"
+              :color="skill.color"
+            />
+          </div>
+        </div>
+        <div>
+          <h3 class="mb-4 text-xl font-semibold">Soft skills</h3>
+          <div class="space-y-4">
+            <Skill
+              v-for="skill in skills.soft"
+              :key="skill.title"
+              :title="skill.title"
+              :icon="skill.icon"
+              :description="skill.description"
+              :color="skill.color"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const skills = {
+  hard: [
+    // 50 word lorem ipsum :
+    {
+      title: "JavaScript",
+      icon: "i-simple-icons-javascript",
+      color: "#f7df1e",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "Python",
+      icon: "i-simple-icons-python",
+      color: "#306998",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ],
+  technologies: [
+    {
+      title: "Vue.js",
+      icon: "i-simple-icons-vuedotjs",
+      color: "#4FC08D",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "Tailwind",
+      icon: "i-simple-icons-tailwindcss",
+      color: "#06b6d4",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ],
+  soft: [
+    {
+      title: "Communication",
+      icon: "i-heroicons-chat-bubble-oval-left",
+      color: "#0284c7",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+    {
+      title: "Teamwork",
+      icon: "i-heroicons-user-group-16-solid",
+      color: "#f43f5e",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    },
+  ],
+};
+</script>
 
 <style scoped></style>
