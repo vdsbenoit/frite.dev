4daf10da9925cb68d58f2048be89089a68505741
vdsbenoit <vdsbenoit@gmail.com>
2024-09-06 21:02:18 +0200

    Improve Skills and Skill components

--- components/Skill.vue
+++ components/Skill.vue
@@ -1,41 +1,47 @@
 <template>
-  <div class="relative inline-block">
-    <div
-      class="group cursor-pointer"
-      @click="showDescription = true"
-      ref="currentElement"
-    >
+  <div class="relative" ref="currentElement">
+    <div>
+      <!-- Invisible div that cover the whole parent div, to increase the hover area -->
+      <!-- Creating a group with the parent div induced weird hover effects with the translations -->
+      <div
+        class="peer absolute bottom-0 left-0 right-0 top-0 z-20 cursor-pointer"
+        @click="showDescription = true"
+      ></div>
       <!-- Icon -->
       <div
-        class="flex size-16 scale-90 items-center justify-center rounded-full bg-gray-800 ease-in-out group-hover:z-10 group-hover:-translate-y-4 group-hover:scale-100 motion-safe:transition-transform"
+        id="icon"
+        class="flex size-16 scale-100 items-center justify-center rounded-full border border-gray-800 bg-gray-900 ease-in-out peer-hover:z-10 peer-hover:-translate-y-4 peer-hover:scale-110 motion-safe:transition-all"
       >
         <UIcon class="size-8" :name="icon" :style="{ color }" />
       </div>
       <!-- Caption -->
       <div
-        class="absolute bottom-0 left-1/2 -translate-x-1/2 text-center opacity-0 ease-in-out group-hover:z-10 group-hover:translate-y-4 group-hover:opacity-100 motion-safe:transition"
+        class="absolute bottom-0 left-1/2 -translate-x-1/2 text-nowrap rounded p-1 text-center text-sm text-gray-800 opacity-0 ease-in-out peer-hover:z-10 peer-hover:translate-y-4 peer-hover:opacity-100 motion-safe:transition"
+        :style="{ backgroundColor: color }"
       >
-        <span
-          class="rounded p-1 text-sm text-gray-800"
-          :style="{ backgroundColor: color }"
-        >
-          {{ title }}
-        </span>
+        {{ title }}
       </div>
     </div>
     <!-- Description popup -->
     <div
       id="description"
-      class="absolute left-1/2 top-1/2 z-10 h-auto w-72 -translate-x-1/2 -translate-y-1/2 rounded bg-gray-100 p-4 text-base text-gray-800 motion-safe:transition-transform"
+      class="absolute left-1/2 top-1/2 z-30 -translate-x-1/2 -translate-y-1/2 rounded bg-gray-300 p-4 text-base text-gray-900 motion-safe:transition-transform"
       :class="{ 'scale-100': showDescription, 'scale-0': !showDescription }"
     >
-      <h4
-        class="mx-auto inline rounded px-3 py-1 font-bold text-gray-800"
-        :style="{ backgroundColor: color }"
-      >
-        {{ title }}
-      </h4>
-      <p class="mt-3">{{ description }}</p>
+      <div class="flex items-center justify-between">
+        <h4
+          class="rounded px-4 py-1 font-bold text-gray-800"
+          :style="{ backgroundColor: color }"
+        >
+          {{ title }}
+        </h4>
+        <UIcon
+          class="size-7 cursor-pointer pr-4"
+          name="i-heroicons-x-mark-16-solid"
+          @click="showDescription = false"
+        />
+      </div>
+      <p class="mt-3 h-auto w-60">{{ description }}</p>
     </div>
   </div>
 </template>
--- components/section/Skills.vue
+++ components/section/Skills.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="flex min-h-screen items-center justify-center px-4">
-    <div class="relative max-w-screen-lg">
+    <div class="relative max-w-screen-lg px-2">
       <div class="mt-4 py-4" v-for="section in sections" :key="section.id">
-        <h2 class="mb-2 text-left text-5xl font-bold">{{ section.title }}</h2>
+        <h2 class="text-left text-5xl font-bold">{{ section.title }}</h2>
         <div
           class="grid grid-cols-1 gap-4"
           :class="[`sm:grid-cols-${section.content.groups.length}`]"
@@ -11,10 +11,10 @@
             v-for="group in section.content.groups"
             :key="`${group.id}-subsection`"
           >
-            <h3 class="mb-4 text-xl font-semibold">
+            <h3 class="mb-4 mt-4 text-xl font-semibold">
               {{ group.title }}
             </h3>
-            <div class="spacey1 space-x-1">
+            <div class="flex flex-wrap gap-1">
               <Skill
                 v-for="skill in section.content.items.filter(
                   (item: any) => item.group === group.id,
