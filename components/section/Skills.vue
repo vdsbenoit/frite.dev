ce08fe069b9b02225b9160bb9c365b00cc4324bd
vdsbenoit <vdsbenoit@gmail.com>
2024-09-09 17:26:13 +0200

    Reorganize skills.yaml

--- components/section/Skills.vue
+++ components/section/Skills.vue
@@ -1,26 +1,25 @@
 <template>
   <div class="flex min-h-screen items-center justify-center px-4">
     <div class="relative max-w-screen-lg px-2">
-      <div class="mt-4 py-4" v-for="section in sections" :key="section.id">
-        <h2 class="text-left text-5xl font-bold">{{ section.title }}</h2>
+      <div class="mt-4 py-4" v-for="subSection in content" :key="subSection.id">
+        <h2 class="text-left text-5xl font-bold">{{ subSection.title }}</h2>
         <div
           class="grid grid-cols-1 gap-4"
-          :class="[`sm:grid-cols-${section.content.groups.length}`]"
+          :class="[`sm:grid-cols-${subSection.groups.length}`]"
         >
           <div
-            v-for="group in section.content.groups"
-            :key="`${group.id}-subsection`"
+            v-for="group in subSection.groups"
+            :key="`skill-${group.id}`"
+            :id="`skill-${group.id}`"
           >
             <h3 class="mb-4 mt-4 text-xl font-semibold">
               {{ group.title }}
             </h3>
             <div class="flex flex-wrap gap-1">
               <Skill
-                v-for="skill in section.content.items.filter(
-                  (item: any) => item.group === group.id,
-                )"
-                :key="skill.title"
-                :id="skill.id"
+                v-for="skill in group.items"
+                :key="`skill-${skill.id}`"
+                :id="`skill-${skill.id}`"
                 :title="skill.title"
                 :icon="skill.icon ?? undefined"
                 :description="skill.description"
@@ -35,12 +34,6 @@
 </template>
 
 <script lang="ts" setup>
-import skills from "~/assets/skills.yaml";
-import techs from "~/assets/techs.yaml";
-
-const sections = [
-  { title: "Skills", id: "skills-subsection", content: skills },
-  { title: "Techologies", id: "techs-subsection", content: techs },
-];
+import content from "~/assets/skills.yaml";
 </script>
 <style scoped></style>
--- tests/Skills.spec.ts
+++ tests/Skills.spec.ts
@@ -1,26 +1,15 @@
 import { describe, it, expect } from "vitest";
 import skills from "~/assets/skills.yaml";
-import techs from "~/assets/techs.yaml";
 
 describe.concurrent("Skills component", () => {
   it("should not have duplicate item ids", () => {
-    [skills, techs].forEach((section: any) => {
-      const ids = section.items.map((item: any) => item.id);
-      const duplicateIds = ids.filter(
-        (id: string, index: number) => ids.indexOf(id) !== index,
-      );
-      expect(duplicateIds).toStrictEqual([]);
-    });
-  });
-
-  it("should not have items with a group that is not listed in groups", () => {
-    [skills, techs].forEach((section: any) => {
-      const groupIds = section.groups.map((group: any) => group.id);
-      const invalidItems = section.items.filter(
-        (item: any) => !groupIds.includes(item.group),
-      );
-      const invalidGroups = invalidItems.map((item: any) => item.group);
-      expect(invalidGroups).toStrictEqual([]);
-    });
+    const ids: string[] = skills
+      .flatMap((subSection: any) => subSection.groups)
+      .flatMap((group: any) => group.items)
+      .map((item: any) => item.id);
+    const duplicateIds = ids.filter(
+      (id: string, index: number) => ids.indexOf(id) !== index,
+    );
+    expect(duplicateIds).toStrictEqual([]);
   });
 });
