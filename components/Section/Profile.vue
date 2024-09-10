345e5f35f8a4a1b5a5f708c8053db0175736bc9b
vdsbenoit <vdsbenoit@gmail.com>
2024-09-10 17:31:53 +0200

    Progress profile page

--- components/Section/Profile.vue
+++ components/Section/Profile.vue
@@ -1,64 +1,101 @@
 <template>
-  <div class="max-w-screen-lg px-6">
-    <h2 class="">Hi, I am Benoit</h2>
-    <div class="grid grid-cols-1 gap-4 sm:grid-cols-5">
-      <!-- First Column -->
-      <div class="col-span-1 flex flex-col items-center sm:col-span-2">
-        <img
-          src="~/assets/img/avatar.jpg"
-          alt="Avatar"
-          class="mb-4 size-72 rounded-full"
-        />
-        <h2 class="text-xl font-semibold">@vdsbenoit</h2>
-        <div class="mt-4 flex space-x-4">
-          <a href="https://github.com/vdsbenoit" target="_blank">
-            <UIcon name="i-simple-icons-github" class="text-2xl" />
-          </a>
-          <a href="https://gitlab.com/vdsbenoit" target="_blank">
-            <UIcon name="i-vscode-icons-file-type-gitlab" class="text-2xl" />
-          </a>
-          <a href="https://linkedin.com/in/vdsbenoit" target="_blank">
-            <UIcon name="i-logos-linkedin-icon" class="text-2xl" />
-          </a>
+  <div class="flex min-h-screen items-center justify-center">
+    <div class="mt-4 max-w-screen-lg px-2 py-6">
+      <div class="grid grid-cols-1 gap-4 sm:grid-cols-5">
+        <!-- First Column -->
+        <div
+          class="col-span-1 flex flex-col items-center gap-y-2 sm:col-span-2"
+        >
+          <img
+            src="~/assets/img/avatar.jpg"
+            alt="Avatar"
+            class="size-64 rounded-full"
+          />
+          <p class="font-mono">@vdsbenoit</p>
+          <div
+            class="flex items-center justify-center gap-x-4 *:transition hover:*:-translate-y-1 hover:*:scale-110"
+          >
+            <NuxtLink to="https://github.com/vdsbenoit" target="_blank">
+              <UIcon name="i-simple-icons-github" class="text-2xl" />
+            </NuxtLink>
+            <NuxtLink to="https://gitlab.com/vdsbenoit" target="_blank">
+              <UIcon name="i-vscode-icons-file-type-gitlab" class="text-2xl" />
+            </NuxtLink>
+            <NuxtLink to="https://linkedin.com/in/vdsbenoit" target="_blank">
+              <UIcon name="i-logos-linkedin-icon" class="text-2xl" />
+            </NuxtLink>
+            <NuxtLink to="mailto:benoit@frite.dev" target="_blank">
+              <UIcon name="i-logos-google-gmail" class="text-xl" />
+            </NuxtLink>
+          </div>
+          <UButton
+            class="transition hover:scale-110"
+            size="md"
+            color="primary"
+            variant="solid"
+            icon="i-heroicons-arrow-down-circle-16-solid"
+            label="Go to skills section"
+            to="#skills"
+            >My skills</UButton
+          >
         </div>
-      </div>
 
-      <!-- Second Column -->
-      <div class="col-span-1 sm:col-span-3">
-        <p class="mb-6">
-          This is a placeholder description for the profile. It gives a brief
-          overview of the individual's professional background and achievements.
-        </p>
-        <div class="space-y-6">
-          <div>
-            <h3 class="text-lg font-semibold">2023 - Present</h3>
-            <p class="text-sm">1 year</p>
-            <h4 class="text-md font-medium">Senior Developer</h4>
-            <p class="text-sm">Company XYZ</p>
-          </div>
-          <div>
-            <h3 class="text-lg font-semibold">2021 - 2023</h3>
-            <p class="text-sm">2 years</p>
-            <h4 class="text-md font-medium">Mid-level Developer</h4>
-            <p class="text-sm">Company ABC</p>
-          </div>
-          <div>
-            <h3 class="text-lg font-semibold">2019 - 2021</h3>
-            <p class="text-sm">2 years</p>
-            <h4 class="text-md font-medium">Junior Developer</h4>
-            <p class="text-sm">Company DEF</p>
-          </div>
-          <div>
-            <h3 class="text-lg font-semibold">2017 - 2019</h3>
-            <p class="text-sm">2 years</p>
-            <h4 class="text-md font-medium">Intern</h4>
-            <p class="text-sm">Company GHI</p>
-          </div>
-          <div>
-            <h3 class="text-lg font-semibold">2015 - 2017</h3>
-            <p class="text-sm">2 years</p>
-            <h4 class="text-md font-medium">Student</h4>
-            <p class="text-sm">University JKL</p>
+        <!-- Second Column -->
+        <div class="col-span-1 sm:col-span-3">
+          <span class="font-bold">Hi, I am</span><br />
+          <h2
+            class="mb-4 bg-gradient-to-t from-gray-400 to-gray-100 bg-clip-text text-transparent"
+          >
+            Benoit
+          </h2>
+          <p class="mb-6">
+            I am a
+            <span class="decoration-primary underline underline-offset-2"
+              >freelance developer.</span
+            >
+            I find solutions to business issues using technology. I have more
+            than
+            <span class="decoration-primary underline underline-offset-2"
+              >{{ workTimeYears }} years of experience</span
+            >
+            in software development, testing, automation and DevOps. I have
+            worked in
+            <span class="decoration-primary underline underline-offset-2"
+              >various industries</span
+            >
+            such as health, education, entertainment and new technologies.
+          </p>
+          <div class="space-y-6">
+            <div>
+              <h3 class="text-lg font-semibold">2023 - Present</h3>
+              <p class="text-sm">1 year</p>
+              <h4 class="text-md font-medium">Senior Developer</h4>
+              <p class="text-sm">Company XYZ</p>
+            </div>
+            <div>
+              <h3 class="text-lg font-semibold">2021 - 2023</h3>
+              <p class="text-sm">2 years</p>
+              <h4 class="text-md font-medium">Mid-level Developer</h4>
+              <p class="text-sm">Company ABC</p>
+            </div>
+            <div>
+              <h3 class="text-lg font-semibold">2019 - 2021</h3>
+              <p class="text-sm">2 years</p>
+              <h4 class="text-md font-medium">Junior Developer</h4>
+              <p class="text-sm">Company DEF</p>
+            </div>
+            <div>
+              <h3 class="text-lg font-semibold">2017 - 2019</h3>
+              <p class="text-sm">2 years</p>
+              <h4 class="text-md font-medium">Intern</h4>
+              <p class="text-sm">Company GHI</p>
+            </div>
+            <div>
+              <h3 class="text-lg font-semibold">2015 - 2017</h3>
+              <p class="text-sm">2 years</p>
+              <h4 class="text-md font-medium">Student</h4>
+              <p class="text-sm">University JKL</p>
+            </div>
           </div>
         </div>
       </div>
@@ -66,6 +103,9 @@
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+const workTime = new Date().getTime() - new Date(2016, 8).getTime();
+const workTimeYears = Math.floor(workTime / (1000 * 60 * 60 * 24 * 365.25));
+</script>
 
 <style scoped></style>
