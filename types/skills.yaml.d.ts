ecbf1c061dca86294cc2b440b4e91ffdc3858fc3
vdsbenoit <vdsbenoit@gmail.com>
2024-09-11 22:21:25 +0200

    Create type declaration for skills.yaml

---
+++ types/skills.yaml.d.ts
@@ -0,0 +1,24 @@
+interface Item {
+  title: string;
+  id: string;
+  icon: string;
+  color: string;
+  description: string;
+}
+
+interface Group {
+  title: string;
+  id: string;
+  items: Item[];
+}
+
+interface Section {
+  title: string;
+  id: string;
+  groups: Group[];
+}
+
+declare module "*/skills.yaml" {
+  const content: Section[];
+  export default content;
+}
