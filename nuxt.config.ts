1e1ed413a27322e5b258237aeaae05c14cba00e9
vdsbenoit <vdsbenoit@gmail.com>
2024-09-19 17:10:00 +0200

    Add Nuxt Security

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ import yaml from "@rollup/plugin-yaml"
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/test-utils/module", "@nuxt/eslint"],
+  modules: ["@nuxt/ui", "@nuxt/test-utils/module", "@nuxt/eslint", "nuxt-security"],
   vite: { plugins: [yaml()] },
   typescript: {
     typeCheck: "build",
