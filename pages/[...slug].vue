8b8cd0ac2c906901d4649af04d2787c201cea94f
vdsbenoit <vdsbenoit@gmail.com>
2024-09-24 01:26:08 +0200

    Custom 404 page

---
+++ pages/[...slug].vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="flex h-screen flex-col items-center justify-center gap-y-2 sm:justify-start">
+    <h1 class="text-8xl font-bold sm:mt-56">404</h1>
+    <p class="text-xl">Page not found</p>
+    <a
+      href="/"
+      class="bg-primary-400 focus:ring-primary-200 hover:bg-primary-300 active:bg-primary-500 mt-4 inline-block rounded-lg px-4 py-2 text-center font-semibold text-gray-800 shadow-xl transition ease-in focus:outline-none focus:ring-2 motion-safe:hover:scale-110"
+    >
+      Go back home
+    </a>
+  </div>
+</template>
+<script lang="ts" setup></script>
+<style scoped></style>
