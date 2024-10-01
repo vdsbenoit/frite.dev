2a03b57e23eac1bd485d803388314e28a3dc9046
vdsbenoit <vdsbenoit@gmail.com>
2024-10-02 01:53:17 +0200

    Fix tests

--- tests/Skills.spec.ts
+++ tests/Skills.spec.ts
@@ -1,12 +1,13 @@
 import { describe, it, expect } from "vitest"
 import skills from "~/assets/skills.yaml"
+import type { SkillSection, SkillGroup, SkillItem } from "~/types/skills.interfaces.ts"
 
 describe.concurrent("Skills component", () => {
   it("should not have duplicate item ids", () => {
     const ids: string[] = skills
-      .flatMap((subSection: Section) => subSection.groups)
-      .flatMap((group: Group) => group.items)
-      .map((item: Item) => item.id)
+      .flatMap((subSection: SkillSection) => subSection.groups)
+      .flatMap((group: SkillGroup) => group.items)
+      .map((item: SkillItem) => item.id)
     const duplicateIds = ids.filter((id: string, index: number) => ids.indexOf(id) !== index)
     expect(duplicateIds).toStrictEqual([])
   })
